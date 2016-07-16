4e2d346dccb69ca57e10a696ffba49f651430d0b
Athul Raj <athulbraj@gmail.com>
2016-07-16 19:54:05 +0530

    Automatic commit from server

--- server.js
+++ server.js
@@ -7,7 +7,7 @@
 var fs = require('fs');
 var options = {};
 options.ip = '0.0.0.0';
-options.port = parseInt(process.argv[2]) + 80;
+options.port = parseInt(process.argv[2]);
 options.config = { name: 'IEDCenter' };
 // options.https = { key: fs.readFileSync('keys/agent2-key.pem'), cert: fs.readFileSync('keys/agent2-cert.pem')};
 // options.sleep = 2000;
